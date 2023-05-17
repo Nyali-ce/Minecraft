6a4c41308bf03f5c20684a9a9e1ae75eb273c615
Nyali.ce <nathan@alconat.ca>
2023-05-17 18:20:03 -0400

    cleaned up loot tables

--- loot_tables.js
+++ loot_tables.js
@@ -1,9 +1,6 @@
 const fs = require('fs');
 
-const netherite = fs.readFileSync('./datapack/data/reforger/loot_tables/netherite_sword.json', 'utf8')
-
-fs.writeFileSync('./datapack/data/reforger/loot_tables/diamond_sword.json', netherite.replace(/"minecraft:netherite_sword"/g, '"minecraft:diamond_sword"'))
-fs.writeFileSync('./datapack/data/reforger/loot_tables/golden_sword.json', netherite.replace(/"minecraft:netherite_sword"/g, '"minecraft:golden_sword"'))
-fs.writeFileSync('./datapack/data/reforger/loot_tables/iron_sword.json', netherite.replace(/"minecraft:netherite_sword"/g, '"minecraft:iron_sword"'))
-fs.writeFileSync('./datapack/data/reforger/loot_tables/stone_sword.json', netherite.replace(/"minecraft:netherite_sword"/g, '"minecraft:stone_sword"'))
-fs.writeFileSync('./datapack/data/reforger/loot_tables/wooden_sword.json', netherite.replace(/"minecraft:netherite_sword"/g, '"minecraft:wooden_sword"'))
+['diamond_sword', 'golden_sword', 'iron_sword', 'stone_sword', 'wooden_sword'].forEach(loot_table => {
+    fs.writeFileSync(`./datapack/data/reforger/loot_tables/${loot_table}.json`,
+        fs.readFileSync('./datapack/data/reforger/loot_tables/netherite_sword.json', 'utf8').replace(/"minecraft:netherite_sword"/g, `"minecraft:${loot_table}"`))
+})
