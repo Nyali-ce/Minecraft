a8389a571f84a3d4ba3df57b49d602160e57e16c
Nyali.ce <nathan@alconat.ca>
2023-06-29 12:20:45 -0400

    no loot_table catch

--- loot_tables.js
+++ loot_tables.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-['diamond_sword', 'golden_sword', 'iron_sword', 'stone_sword', 'wooden_sword'].forEach(loot_table => {
+(fs.readFileSync('./datapack/data/reforger/loot_tables/netherite_sword.json', 'utf8') ?? '') && ['diamond_sword', 'golden_sword', 'iron_sword', 'stone_sword', 'wooden_sword'].forEach(loot_table => {
     fs.writeFileSync(`./datapack/data/reforger/loot_tables/${loot_table}.json`,
         fs.readFileSync('./datapack/data/reforger/loot_tables/netherite_sword.json', 'utf8').replace(/"minecraft:netherite_sword"/g, `"minecraft:${loot_table}"`))
 })
